f70dce3d9ba032b7a4b468f7c6ebf02086f6ffe6
Erik Nava <erik.nava@gmail.com>
2019-12-09 16:02:34 -0600

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'foo/bar'
   get 'static_pages/home'
   get 'static_pages/help'
-  get  'static_pages/about'
-  root 'application#hello'
+  get 'static_pages/about'
+  get 'static_pages/contact'
+  root 'static_pages#home'
 end
